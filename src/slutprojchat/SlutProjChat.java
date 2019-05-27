644df26f49c2de57863faa034ac837103333afed
Foxmaner <eskil.brann@gmail.com>
2019-05-27 12:16:18 +0200

    Added StartMenu *Added startMenu

--- src/slutprojchat/SlutProjChat.java
+++ src/slutprojchat/SlutProjChat.java
@@ -5,10 +5,15 @@
  */
 package slutprojchat;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.*;
 import javax.swing.*;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
 
 /**
  *
@@ -19,25 +24,39 @@ public class SlutProjChat extends JFrame implements ActionListener{
     /**
      * @param args the command line arguments
      */
-    String namn;
-    InetAddress iadr;
-    int port;
-    MulticastSocket so;
-    JTextArea txt = new JTextArea();
-    JScrollPane sp = new JScrollPane();
-    JTextField skriv = new JTextField();
-    JButton sluta = new JButton("Koppla ner");
-    
+    JButton btnClient = new JButton("New Client");
+    JButton btnServer = new JButton("New Server");
+
+    public SlutProjChat() {
+
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
+        add(Box.createRigidArea(new Dimension(20, 50)));
+        this.add(btnClient);
+        this.add(btnServer);
+        add(Box.createRigidArea(new Dimension(20, 50)));
+
+        btnClient.addActionListener(this);
+        btnServer.addActionListener(this);
+
+        this.pack();
+        this.setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
-        //ballt är coolt
-        //ballare är ballast
-        //Collare
+        SlutProjChat j1 = new SlutProjChat();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (e.getSource()==btnClient) {
+            System.out.println("KlientStartas");
+            new Client();
+        }else if (e.getSource()==btnServer) {
+            System.out.println("ServerStartas");
+            new Server();
+            btnServer.setEnabled(false);
+        }
     }
-    
+
 }
